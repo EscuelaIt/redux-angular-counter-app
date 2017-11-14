37ca109cd5d6434ebda9e31512948aabf551a2af
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-11-14 15:03:53 -0400

    save

--- src/redux/app.reducer.ts
+++ src/redux/app.reducer.ts
@@ -1,6 +1,8 @@
+import { ActionReducerMap } from '@ngrx/store';
+
 import { AppState } from './app.state';
 import { counterReducer } from './counter/counter.reducer';
 
-export const AppReducer = {
+export const AppReducer: ActionReducerMap<AppState> = {
   counter: counterReducer,
 };
